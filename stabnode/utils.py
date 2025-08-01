9d412f0a97ec3774a228ada91e88daf35ec61a79
Ike <54322188+MalachiteWind@users.noreply.github.com>
2025-08-01 10:02:29 -0700

    wip: modeling on transient time data.
    1hr to run 10 epochs is quite slow.
    
    Refactor:
    stabnode/node.py: reorganize file. Removed FTerm and GTerm
    stabnode/utils.py: set_global_seed moved here.
    data.hysteresis.py: improved docstring.

--- stabnode/utils.py
+++ stabnode/utils.py
@@ -1,6 +1,14 @@
 from tqdm.auto import tqdm 
+import torch
 
 def _load_loop_wrapper(show_progress:bool):
     if show_progress:
         return tqdm
     return lambda x: x 
+
+def set_global_seed(seed): 
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
